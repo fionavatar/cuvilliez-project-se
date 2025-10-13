c525d7ebb3dcd6223be1fc42e7fa5c1715b94dfb
Cuvilliez <fiona.cuvilliez@etu.univ-cotedazur.fr>
2025-10-13 22:08:59 +0200

    actualisation du code

--- src/dataCompressing.py
+++ src/dataCompressing.py
@@ -19,8 +19,40 @@ valeur très grande à isoler -> overflow
 
 import math
 
+def decimalToBinary ( liste ) :
+    """
+    Convertit les nombres d'une liste de décimal en binairre
+
+    entrée : une liste d'entiers décimaux
+    sortie : une liste d'entiers binaires
+    """
+    binList = []
+    for elem in liste :
+        nb = elem
+        binNb = ""
+        while nb > 0 :
+            binNb = str(nb%2) + binNb
+            nb = nb // 2
+        binList.append(binNb)
+    return binList
+
+def binaryToDecimal (listeB ) :
+    """
+    Convertit les nombres d'une liste de binaire en décimal
+
+    entrée : une liste d'entiers binaires
+    sortie : une liste d'entiers décimaux
+    """
+    decList = []
+    for elem in listeB :
+        decNb = 0
+        for i in range(len(elem)) :
+            decNb = decNb + int(elem[i])*(2**(len(elem)-1-i))
+        decList.append(decNb)
+    return decList
+
 # trop lent ?
-def nombreBits ( l ) :
+def nombreBits ( liste ) :
     """
     Calcule le nombre de bits nécessaire à la conpression. 
     Astuce on a uniquement besoin de calculer pour l'entier max.
@@ -32,43 +64,27 @@ def nombreBits ( l ) :
     En sortie :
     k : un entier
     """
-    binTab = []
-    k = 0
-    for elem in l :
-
-        if elem == 0 :
-            k = 1
-            binTab.append('0')
-        else :
-            c = elem
-            bin = ""
-            while c > 0 :
-                bin = bin + str(c%2)
-                c = c // 2
-        
-            if len(bin) > k :
-                k = len(bin)
-            binTab.append(''.join(reversed(bin)))
-
-    return k, binTab
+    maxValue = max(liste)
+    if maxValue == 0 :
+        k = 1
+    else :
+        bin = ""
+        while maxValue > 0 :
+            bin = bin + str(maxValue%2)
+            maxValue = maxValue // 2
+            k = len(bin)
+    return k
+    
+    
 
 # plus rapide
-
-def compute_k(array):
-    max_val = max(array)
-    return math.floor(math.log2(max_val)) + 1 if max_val > 0 else 1
-
-
-def get( i ) :
-    """
-    permet l’accès direct à l’élément i sans décompression complète
-
-    Entrée :
-    i un entier
-
-    Sortie :
-    l'élément à l'indice i
-    """
+def calculerK(array):
+    maxValue = max(array)
+    if maxValue == 0 :
+        k = 1
+    else :
+        k = math.floor(math.log2(maxValue)) + 1
+    return k
 
 
 # 1er mode de compression
@@ -80,7 +96,29 @@ crossing allowed — les entiers compressés peuvent s’étendre sur deux mots 
 def compress( input ) :
     """
     compresse int[] input
+
+    entree : input un tableau d'entiers décimaux
+    sortie : un tableau d'entiers décimaux (compressé)
     """
+    listeBin = decimalToBinary(input)
+    k = calculerK(input)
+    nb = math.floor(32/k)
+
+    compressedList = []
+    strBin = ""
+    i = 0
+    for elem in listeBin :
+        if i == k :
+            compressedList.append(strBin)
+            strBin = ""
+            i = 0
+        else :
+            strBin = strBin + elem
+            i+=1
+    compressedList.append(strBin)
+    print(compressedList)
+
+
 
 def decompress( output ) :
     """
@@ -93,14 +131,34 @@ no crossing — chaque entier compressé doit être entièrement contenu
 dans un mot de sortie (donc on peut gaspiller bits et aligner).
 """
 
+def get( i ) :
+    """
+    permet l’accès direct à l’élément i sans décompression complète
+
+    Entrée :
+    i un entier
+
+    Sortie :
+    l'élément à l'indice i
+    """
+
 if __name__ == "__main__":
     values = [5, 3, 7, 1, 2, 6] # Example values (6 values, each 3 bits)
-    data = [ 1, 14, 67, 35, 89]
-    data = [ 12, 25, 31]
+    data = [ 15, 3, 4, 23, 28, 1, 17, 18]
     print(nombreBits([1, 2, 3, 4, 5]))  # ➜ 3
     print(nombreBits([12, 25, 31]))     # ➜ 5
     print(nombreBits([0, 0, 0]))        # ➜ 1
 
-    print(compute_k([1, 2, 3, 4, 5]))  # ➜ 3
-    print(compute_k([12, 25, 31]))     # ➜ 5
-    print(compute_k([0, 0, 0]))        # ➜ 1
+    print(calculerK([1, 2, 3, 4, 5]))  # ➜ 3
+    print(calculerK([12, 25, 31]))     # ➜ 5
+    print(calculerK([0, 0, 0]))        # ➜ 1
+    print(calculerK([ 15, 3, 4, 23, 28, 1, 17, 18]))        # ➜ 7
+
+    print(decimalToBinary ( values ))
+    print(decimalToBinary ( data ))
+    #print(decimalToBinary ( [ 1, 14, 67, 35, 89] ))
+
+    print("il faut :")
+    print(math.floor(32/calculerK([ 15, 3, 4, 23, 28, 1, 17, 18])), "entiers")
+    compress(data)
+    print(binaryToDecimal(['1111111001011111100', '1000110010']))
